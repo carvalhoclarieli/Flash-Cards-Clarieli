7d3bd8c831fdd4eac992a39bf9666a677b285b05
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2024-10-18 12:26:30 -0300

    18/10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia' ,
+    'Qual o único continente com terra nos quatro hemisférios?' ,
+    'É o continente africano'
+)
+
+criaCartao(
+    'Categoria' ,
+    'Pergunta?' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Categoria' ,
+    'Pergunta?' ,
+    'Resposta'
+)
+
+criaCartao(
+    'Categoria' ,
+    'Pergunta?' ,
+    'Resposta'
+)
