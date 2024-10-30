b2b14768f0436254871d80db4682ee81662198cd
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2024-10-30 10:16:12 -0300

    30/10

--- aap.js
+++ aap.js
@@ -17,12 +17,12 @@ function criaCartao(categoria, pergunta, resposta) {
 
     let respostaEstaVisivel = false;
 
-    function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active', respostaEstaVisivel);
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel;
+    cartao.classList.toggle('active', respostaEstaVisivel);
     }
 
-    cartao.addEventListener('click', viraCartao);
+cartao.addEventListener('click', viraCartao);
 
     container.appendChild(cartao);
 }
